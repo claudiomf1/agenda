1b6ed3a2abdd5050167a0701ca4f5d93d8115eba
claudiomf1 <claudiomf1+1@gmail.com>
2022-02-18 10:40:13 -0300

    fist commit

---
+++ util.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+const { dirname } = require("path")
+const path = require("path")
+const servDir = "server"
+const finalDir = "apps"
+const distDir = "dist"
+const htmlFile = "index.html"
+fs.copyFileSync(path.resolve(__dirname, distDir, htmlFile), path.resolve(__dirname, finalDir, htmlFile));
+const dir = fs.readdirSync(path.resolve(__dirname, servDir));
+dir.forEach(f => {
+    fs.copyFileSync(path.resolve(__dirname, servDir, f), path.resolve(__dirname, finalDir, f))
+})
