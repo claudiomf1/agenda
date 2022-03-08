0e69404e62e5bc2883c1e24a7003a975fae59f37
mayaemerson <emersonmaia@me.com>
2022-03-08 08:57:34 -0300

    criando pagues html

---
+++ server/model.js
@@ -0,0 +1,21 @@
+const idUs = "1x9nYtfXOVQuQgVZJtizlLhea8ODMf4NSbSq9UarZMTM"
+const idBd = "1Jf0GBVd0Drafa8QXRZOsoIxnQwQFSAMm3VIgIgo5f3c"
+const sb = SpreadsheetApp.openById (idBd)
+const su = SpreadsheetApp.openById (idUs)
+
+var wat = sb.getSheetByName("atendimento")
+var wcg = sb.getSheetByName("cadastroGeral")
+var wcs = sb.getSheetByName("cadastroServico")
+/*user*/
+var wus = su.getSheetByName("user")
+var wul = su.getSheetByName("login")
+
+
+var chave = "sdfsdf09f789sdf78sdf3453sdfsdhnxd9849sdf78sdf3453sdhhpwewemzxer849958sdf3453sdfsdf0pxneodf3453vcchinb94V5948527784HdmeXXxrime38994000303030272663677882lmsoekkowjhgowsermkelwsecvcrendAAA0w0w0oMMheower352342odllBBroenowerm3859990003020094933284773uyemkeot23"
+var senha = ""
+
+
+function cont(){
+
+
+}
