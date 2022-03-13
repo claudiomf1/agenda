e95fe3ba416d64c68f60b240bf131f10f68fdd72
mayaemerson <emersonmaia@me.com>
2022-03-13 11:30:02 -0300

    org pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 
 import { callHearder, callBody, callLogin, callHprincipal, callManutencao, callRegisterList, callRegisterI, callCdUser, callPedido, callnewPedido } from './caldpages'
-import { btnClick} from './scripts/login_client'
+import { loginAut } from './login_client'
+  
+
 
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("body-home").addEventListener('click', btnClick)
@@ -57,3 +59,42 @@ let masks = {
   }
 }
 
+
+
+ const btnClick = (e) => { 
+
+  const inputNome = document.getElementById("username")
+  const inputPswd = document.getElementById("password")
+  const mensagemL = document.getElementById("msn-login")
+  
+  const senhaUser = document.getElementById("rs-user-senha")
+  const confSenhaUser = document.getElementById("rs-user-conf-senha")
+
+ if(e.target.matches('#btn-lg')){
+   $("#bar").show()
+   callLogin()
+   $("#title-page").html("Login")
+   $("#bar").hide()
+}
+
+if(e.target.matches('#btn-login')){
+ if(inputNome.value === ""){
+   $(mensagemL).html("O campo e-mail não pode ser em branco...")
+   inputNome.value = "" 
+   inputNome.focus()
+   setTimeout(function() {
+     $(mensagemL).html("Faça login com sua conta de e-mail")
+   },5000)
+  }else if(inputPswd.value === ""){
+   $(mensagemL).html("O campo senha não pode ser em branco...")
+   inputPswd.value = "" 
+   inputPswd.focus()
+   setTimeout(function() {
+     $(mensagemL).html("Faça login com sua conta de e-mail")
+   },5000)
+
+  }else {
+   loginAut()
+  }
+}
+}//fim btn click
