5aae3ecfebb50a925aad566fd69cb1346d272bd4
mayaemerson <emersonmaia@me.com>
2022-03-15 17:02:12 -0300

    btn login

--- client/js/main.js
+++ client/js/main.js
@@ -5,28 +5,23 @@ import { callHearder, callBody } from './caldpages'
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  //document.getElementById("body-home").addEventListener('click', btnClick)
+  document.getElementById("body-home").addEventListener('click', btnClick)
   
 
  $("#bar").hide()
 
- var MenuItems = document.getElementById("MenuItems");
 
- MenuItems.style.maxHeight = "0px";
-
- function menutoggle() {
-     if (MenuItems.style.maxHeight == "0px") {
-         MenuItems.style.maxHeight = "200px";
-     }
-     else {
-         MenuItems.style.maxHeight = "0px";
-     }
- }
-
+ 
  
  
 
 })
+
+window.onload = function(){
+ 
+  callBody()
+
+}
 
   /*let    inputNome = document.getElementById("username")
   let    inputPswd = document.getElementById("password")
@@ -35,11 +30,16 @@ document.addEventListener("DOMContentLoaded", function() {
   let    confSenhaUser = document.getElementById("rs-user-conf-senha")*/
 
 
-window.onload = function(){
- 
-     callBody()
-  
-}
+  const btnClick = (e) => {
+
+    if(e.target.matches('#btn_pg_Login')){ 
+
+      alert ('Teste clicando em login')
+      
+      }
+
+
+  }
 
 
 
