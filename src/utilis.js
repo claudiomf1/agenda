a0241e2a18fe2551572b211003d397f99f83693c
claudiomf1 <claudiomf1+1@gmail.com>
2022-04-05 19:09:04 -0300

    feito responsivo de todo o site e medida px mudado para rem com porcentagem

--- src/utilis.js
+++ src/utilis.js
@@ -1,16 +1,14 @@
-
-     
 function render(file, argoObject) {
-  var template = HtmlService.createTemplateFromFile(file); 
-  if (argoObject) {
-      var keys = Object.keys(argoObject);
+    var template = HtmlService.createTemplateFromFile(file);
+    if (argoObject) {
+        var keys = Object.keys(argoObject);
 
-      keys.forEach(function (key) {
-          template[key] = argoObject[key];
-      });
-  } //end if
-  return template.evaluate().setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
-   //.addMetaTag('viewport', 'width=device-width, initial-scale=1')
-   .setTitle("Modelagem- Home")
-   .setFaviconUrl('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Paris_RER_D_icon.svg/512px-Paris_RER_D_icon.svg.png')
+        keys.forEach(function(key) {
+            template[key] = argoObject[key];
+        });
+    } //end if
+    return template.evaluate().setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+        //.addMetaTag('viewport', 'width=device-width, initial-scale=1')
+        .setTitle("Modelagem- Home")
+        .setFaviconUrl('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Paris_RER_D_icon.svg/51.2rem-Paris_RER_D_icon.svg.png')
 }
